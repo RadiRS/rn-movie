8c18d91dc4d132758d743b6005bd1d4c803924cf
Radi Rusadi <radi.rusadi.s@gmail.com>
2022-10-18 18:59:34 +0700

    Initial commit

---
+++ src/components/ui/button/button.component.tsx
@@ -0,0 +1,61 @@
+import React, { FC, ReactNode } from 'react';
+import {
+  TouchableOpacity,
+  TouchableOpacityProps,
+  ViewStyle,
+  GestureResponderEvent,
+} from 'react-native';
+
+import { useTheme } from '@/hooks';
+import { Text, Spinner } from '@/components/ui';
+import { ButtonAppearances, ButtonSizes, ButtonStatus } from './button.types';
+import styles from './button.styles';
+
+export interface ButtonProps extends TouchableOpacityProps {
+  children?: ReactNode;
+  status?: ButtonStatus;
+  size?: ButtonSizes;
+  appearance?: ButtonAppearances;
+  loading?: boolean | null | undefined;
+  disabled?: boolean | undefined;
+  style?: ViewStyle;
+  onPress?: ((event: GestureResponderEvent) => void) | undefined;
+}
+
+const Button: FC<ButtonProps> = ({
+  children,
+  loading,
+  style,
+  onPress,
+  ...props
+}: ButtonProps) => {
+  const theme = useTheme();
+  const isString = typeof children === 'string';
+
+  const s = styles({ theme, props });
+
+  const extOnPress = (event: GestureResponderEvent): void => {
+    if (loading) {
+      return;
+    }
+
+    onPress && onPress(event);
+  };
+
+  return (
+    <TouchableOpacity
+      activeOpacity={0.9}
+      style={[s.btn, style]}
+      onPress={extOnPress}
+      {...props}>
+      {loading ? (
+        <Spinner isVisible size="small" color={theme.Colors.alternative} />
+      ) : (
+        isString && <Text style={s.text}>{children}</Text>
+      )}
+      {!isString && children}
+    </TouchableOpacity>
+  );
+};
+
+export default Button;
---
+++ src/components/ui/text/text.types.ts
@@ -0,0 +1,23 @@
+export type TextTypes = 'bold' | 'light' | 'italic' | 'regular' | 'semi-bold';
+
+export type TextStatus =
+  | 'basic'
+  | 'primary'
+  | 'success'
+  | 'info'
+  | 'warning'
+  | 'error'
+  | 'disabled'
+  | 'control';
+
+export type TextAppearances = 'default' | 'alternative' | 'hint';
+
+export type TextAlign = 'center' | 'left' | 'right';
+
+export type TextVariants =
+  | 'small'
+  | 'regular'
+  | 'large'
+  | 'title-small'
+  | 'title-regular'
+  | 'title-large';
