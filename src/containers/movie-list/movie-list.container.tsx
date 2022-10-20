b4fcab675332b4f135a7533fbc81e6737acb51a6
Radi Rusadi <radi.rusadi.s@gmail.com>
2022-10-20 10:57:46 +0700

    Added infinite scroll at movie-list container

--- src/containers/movie-list/movie-list.container.tsx
+++ src/containers/movie-list/movie-list.container.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { IMG_BASE_URL } from '@env';
 import { StyleSheet, Pressable, Image, ListRenderItem } from 'react-native';
 import { useRoute } from '@react-navigation/native';
@@ -6,7 +6,7 @@ import { useRoute } from '@react-navigation/native';
 import { navigate } from '@/navigators';
 import { useTheme } from '@/hooks';
 import { Movie, useGetMoviesQuery } from '@/services/movie';
-import { Text, View, SafeArea, FlatList } from '@/components/ui';
+import { Text, View, SafeArea, FlatList, Spinner } from '@/components/ui';
 
 interface MovieListContainerProps {
   params: { path: string };
@@ -18,8 +18,39 @@ const MovieListContainer = () => {
   const router = useRoute<MovieListContainerProps>();
   const styles = useStyles();
   const path = router.params?.path;
+  const [page, setPage] = useState(1);
+  const [movies, setMovies] = useState<Movie[]>([]);
 
-  const { data, isSuccess } = useGetMoviesQuery({ path: path });
+  const { data, isFetching, isLoading } = useGetMoviesQuery({
+    path: path,
+    page,
+  });
+
+  useEffect(() => {
+    if (!data?.results) {
+      return;
+    }
+
+    if (page === 1) {
+      setMovies(data?.results);
+    } else {
+      setMovies(prevState => [...prevState, ...data.results]);
+    }
+
+    return () => {};
+  }, [page, isFetching, data?.results]);
+
+  const onRefresh = () => {
+    setPage(1);
+  };
+
+  const onEndReached = () => {
+    if (!data || page >= data?.total_pages || isFetching) {
+      return;
+    }
+
+    setPage(prevPage => prevPage + 1);
+  };
 
   const onPressItem = (item: Movie) => {
     navigate('MovieDetail', { id: item.id });
@@ -49,20 +80,30 @@ const MovieListContainer = () => {
     );
   };
 
+  const renderFooter = () => {
+    if (!isFetching) {
+      return null;
+    }
+
+    return <Spinner size="small" />;
+  };
+
   return (
     <SafeArea style={styles.container}>
-      {isSuccess && (
-        <FlatList
-          data={data?.results}
-          renderItem={renderItem}
-          style={styles.listContentContainer}
-        />
-      )}
+      <FlatList
+        data={movies}
+        style={styles.listContentContainer}
+        renderItem={renderItem}
+        ListFooterComponent={renderFooter}
+        onEndReached={onEndReached}
+        onRefresh={onRefresh}
+        refreshing={isLoading}
+        onEndReachedThreshold={0.3}
+      />
+      <Spinner isFull={isLoading} />
     </SafeArea>
   );
 };
-
-export default MovieListContainer;
 
 const useStyles = () => {
   const { MetricsSizes } = useTheme();
@@ -88,3 +129,5 @@ const useStyles = () => {
     },
   });
 };
+
+export default MovieListContainer;
